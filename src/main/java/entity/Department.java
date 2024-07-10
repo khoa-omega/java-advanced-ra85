a080fb432e7790b40a1f6df7eebc4fc9b396241e
Khoa <nvkhoa05@gmail.com>
2024-07-10 21:30:05 +0700

    Create

--- src/main/java/entity/Department.java
+++ src/main/java/entity/Department.java
@@ -5,10 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PostPersist;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -23,4 +29,22 @@ public class Department {
 
     @Column(name = "name", length = 50, unique = true, nullable = false)
     private String name;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        System.out.println("Trước khi thêm vào database");
+    }
+
+    @PostPersist
+    public void postPersist() {
+        System.out.println("Sau khi thêm vào database");
+    }
 }
