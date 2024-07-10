c3250ea8695c421479683abcf48156f49138f94b
Khoa <nvkhoa05@gmail.com>
2024-07-10 21:04:21 +0700

    Create

---
+++ src/main/java/Program.java
@@ -0,0 +1,54 @@
+import entity.Department;
+import util.HibernateUtil;
+
+public class Program {
+    public static void main(String[] args) {
+        try (var factory = HibernateUtil.buildSessionFactory()) {
+            factory.inTransaction(session -> {
+                var department = new Department();
+                department.setName("Giám đốc");
+                session.persist(department);
+            });
+            factory.inTransaction(session -> {
+                var department = new Department();
+                department.setName("Bảo vệ");
+                session.persist(department);
+            });
+
+            factory.inSession(session -> {
+                var hql = "FROM Department";
+                var departments = session
+                        .createSelectionQuery(hql, Department.class)
+                        .getResultList();
+                for (var department : departments) {
+                    System.out.println("🎯 department = " + department);
+                }
+            });
+
+            factory.inSession(session -> {
+                var department = session.get(Department.class, 1);
+                System.out.println("⭐ department = " + department);
+            });
+
+            factory.inSession(session -> {
+                var hql = "FROM Department WHERE name = :name";
+                var department = session
+                        .createSelectionQuery(hql, Department.class)
+                        .setParameter("name", "Bảo vệ")
+                        .uniqueResult();
+                System.out.println("🍒 department = " + department);
+            });
+
+            factory.inTransaction(session -> {
+                var department = session.get(Department.class, 2);
+                department.setName("Kinh doanh");
+                session.merge(department);
+            });
+
+            factory.inTransaction(session -> {
+                var department = session.get(Department.class, 1);
+                session.remove(department);
+            });
+        }
+    }
+}
---
+++ src/main/java/entity/Department.java
@@ -0,0 +1,26 @@
+package entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "department")
+public class Department {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", length = 50, unique = true, nullable = false)
+    private String name;
+}
